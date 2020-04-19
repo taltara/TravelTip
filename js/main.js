5d4942da3556809721ea1c4cb059b347f5bebdd6
root <root@DESKTOP-287NC8P.localdomain>
2020-04-19 16:19:28 +0300

    shlomo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-import { utilService } from './services/util-services';
-import { locationServices } from './services/location-services';
+import { utilService } from './services/util-services.js';
+import { locationServices } from './services/location-services.js';
 
 window.addEventListener('load', onInit);
+var map;
 
 function onInit() {
+    
+    initMap();
     bindEvents();
     render();
 }
@@ -19,4 +22,26 @@ function render() {
 function renderLocations() {
   
 
+}
+
+function initMap(lat = null, lng = null) {
+    console.log('are you here');
+    var centerOn;
+    if (!lat || !lng) {
+        // Eilat
+        centerOn = { lat: 29.5577, lng: 34.9519 };
+    } else {
+
+        centerOn = { lat, lng };
+    }
+    var elMap = document.querySelector('#map');
+    
+    var options = { center: centerOn, zoom: 10, gestureHandling: 'greedy' };
+    map = new google.maps.Map(elMap, options);
+
+
+    // map.addListener('click', function(e) {
+
+    //     onNewLocation(e);
+    // });
 }
